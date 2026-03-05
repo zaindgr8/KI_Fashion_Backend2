022b123485e4e6a364b44a8718d8d470bbe78549
Ahmed Faraz <ahmedfarazdev@gmail.com>
2026-03-05 17:44:50 +0500

    new url added to server.js

--- server.js
+++ server.js
@@ -54,6 +54,8 @@ const productionOrigins = [
   "http://127.0.0.1:3000",
   "http://127.0.0.1:3001",
   "http://127.0.0.1:3002",
+  "http://127.0.0.1:3003",
+  
 ];
 
 // Development/localhost origins (always allowed for local testing)
